b480ab6260da63cc17b3110671a3a647ca049c97
098orin <098orin0102@gmail.com>
2026-02-08 18:13:01 +0900

    Fix auto_height_multiple_children

--- tests/spacing_and_sizing.rs
+++ tests/spacing_and_sizing.rs
@@ -571,7 +571,7 @@ fn auto_height_multiple_children() {
         ..Default::default()
     });
 
-    let mut root = LayoutNode::with_children(
+    let inner = LayoutNode::with_children(
         Style {
             size: SizeStyle {
                 width: Length::Px(200.0),
@@ -583,9 +583,11 @@ fn auto_height_multiple_children() {
         vec![child1, child2],
     );
 
+    let mut root = LayoutNode::with_children(Style::default(), vec![inner]);
+
     LayoutEngine::layout(&mut root, 800.0, 600.0);
 
-    match &root.layout_boxes {
+    match &root.children[0].layout_boxes {
         LayoutBoxes::Single(box_model) => {
             // Parent with Auto height should adopt the largest child's height
             assert_eq!(box_model.content_box.width, 200.0);
