d2670fb5ecaad94a129f0c5e897319aeefec0d99
098orin <098orin0102@gmail.com>
2026-03-28 18:26:44 +0900

    Fix document comment for BoxModel

--- src/geometry.rs
+++ src/geometry.rs
@@ -11,7 +11,7 @@ pub struct Rect {
 /// Represents the layout box model of an element.
 ///
 /// Each field is expressed in absolute coordinates relative to the
-/// `border_box` origin which is always `zero`.
+/// `border_box` origin.
 #[derive(Debug, Clone, Default)]
 pub struct BoxModel {
     /// The outermost box including border.
