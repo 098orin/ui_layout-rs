b63de22c51d5bc0e532efa7be42768a8bbd5d477
098orin <098orin0102@gmail.com>
2026-06-05 20:23:47 +0900

    Update mixed_layout.rs

--- tests/mixed_layout.rs
+++ tests/mixed_layout.rs
@@ -16,49 +16,7 @@ fn fragment(width: f32, height: f32) -> ItemFragment {
 }
 
 #[test]
-fn block_with_inline_then_block_siblings() {
-    let inline_child = LayoutNode::with_children(
-        Style {
-            display: Display::parse("inline").unwrap(),
-            line_height: Length::Px(20.0),
-            ..Default::default()
-        },
-        [fragment(35.0, 15.0)],
-    );
-
-    let block_child = LayoutNode::new(Style {
-        size: SizeStyle {
-            width: LengthOrAuto::Length(Length::Px(100.0)),
-            height: LengthOrAuto::Length(Length::Px(40.0)),
-            ..Default::default()
-        },
-        ..Default::default()
-    });
-
-    let mut root = LayoutNode::with_children(
-        Style {
-            size: SizeStyle {
-                width: LengthOrAuto::Length(Length::Px(200.0)),
-                height: LengthOrAuto::Auto,
-                ..Default::default()
-            },
-            ..Default::default()
-        },
-        [inline_child, block_child],
-    );
-
-    LayoutEngine::layout(&mut root, 800.0, 600.0);
-
-    let inline_boxes: Vec<BoxModel> = node(&root, 0).layout_box.iter().collect();
-    assert_eq!(inline_boxes.len(), 1);
-    assert_eq!(inline_boxes[0].border_box.y, 0.0);
-
-    assert_eq!(block_box(node(&root, 1)).border_box.y, 20.0);
-    assert_eq!(block_box(&root).content_box.height, 60.0);
-}
-
-#[test]
-fn block_with_block_then_inline_siblings() {
+fn block_then_inline() {
     let block_child = LayoutNode::new(Style {
         size: SizeStyle {
             width: LengthOrAuto::Length(Length::Px(100.0)),
@@ -100,23 +58,117 @@ fn block_with_block_then_inline_siblings() {
 }
 
 #[test]
-fn block_with_interleaved_inline_and_block() {
+fn inline_then_block() {
+    let inline_child = LayoutNode::with_children(
+        Style {
+            display: Display::parse("inline").unwrap(),
+            line_height: Length::Px(20.0),
+            ..Default::default()
+        },
+        [fragment(35.0, 15.0)],
+    );
+
+    let block_child = LayoutNode::new(Style {
+        size: SizeStyle {
+            width: LengthOrAuto::Length(Length::Px(100.0)),
+            height: LengthOrAuto::Length(Length::Px(40.0)),
+            ..Default::default()
+        },
+        ..Default::default()
+    });
+
+    let mut root = LayoutNode::with_children(
+        Style {
+            size: SizeStyle {
+                width: LengthOrAuto::Length(Length::Px(200.0)),
+                height: LengthOrAuto::Auto,
+                ..Default::default()
+            },
+            ..Default::default()
+        },
+        [inline_child, block_child],
+    );
+
+    LayoutEngine::layout(&mut root, 800.0, 600.0);
+
+    let inline_boxes: Vec<BoxModel> = node(&root, 0).layout_box.iter().collect();
+    assert_eq!(inline_boxes.len(), 1);
+    assert_eq!(inline_boxes[0].border_box.y, 0.0);
+
+    assert_eq!(block_box(node(&root, 1)).border_box.y, 20.0);
+    assert_eq!(block_box(&root).content_box.height, 60.0);
+}
+
+#[test]
+fn inline_with_fragment_and_block() {
+    let block_child1 = LayoutNode::new(Style {
+        display: Display::parse("block").unwrap(),
+        size: SizeStyle {
+            width: LengthOrAuto::Length(Length::Px(80.0)),
+            height: LengthOrAuto::Length(Length::Px(30.0)),
+            ..Default::default()
+        },
+        ..Default::default()
+    });
+
+    let block_child2 = LayoutNode::new(Style {
+        display: Display::parse("block").unwrap(),
+        size: SizeStyle {
+            width: LengthOrAuto::Length(Length::Px(80.0)),
+            height: LengthOrAuto::Length(Length::Px(30.0)),
+            ..Default::default()
+        },
+        ..Default::default()
+    });
+
+    let inline_wrapper = LayoutNode::with_children(
+        Style {
+            display: Display::parse("inline").unwrap(),
+            line_height: Length::Px(20.0),
+            ..Default::default()
+        },
+        [
+            LayoutChild::from(block_child1),
+            fragment(40.0, 15.0).into(),
+            fragment(30.0, 15.0).into(),
+            LayoutChild::from(block_child2),
+            fragment(30.0, 15.0).into(),
+        ],
+    );
+
+    let mut root = LayoutNode::with_children(
+        Style {
+            size: SizeStyle {
+                width: LengthOrAuto::Length(Length::Px(200.0)),
+                height: LengthOrAuto::Auto,
+                ..Default::default()
+            },
+            ..Default::default()
+        },
+        [inline_wrapper],
+    );
+
+    LayoutEngine::layout(&mut root, 800.0, 600.0);
+
+    assert_eq!(block_box(&root).content_box.height, 100.0);
+
+    let inline_child = node(&root, 0);
+    let inline_box = inline_child.layout_box.iter().collect::<Vec<_>>();
+    assert_eq!(inline_box.len(), 2);
+
+    assert_eq!(inline_child.layout_box.width_box(), 70.0);
+}
+
+#[test]
+fn consecutive_inline() {
     let inline1 = LayoutNode::with_children(
         Style {
             display: Display::parse("inline").unwrap(),
             line_height: Length::Px(20.0),
             ..Default::default()
         },
-        [fragment(30.0, 10.0)],
+        [fragment(35.0, 12.0)],
     );
-
-    let block1 = LayoutNode::new(Style {
-        size: SizeStyle {
-            height: LengthOrAuto::Length(Length::Px(30.0)),
-            ..Default::default()
-        },
-        ..Default::default()
-    });
 
     let inline2 = LayoutNode::with_children(
         Style {
@@ -124,16 +176,8 @@ fn block_with_interleaved_inline_and_block() {
             line_height: Length::Px(20.0),
             ..Default::default()
         },
-        [fragment(40.0, 10.0)],
+        [fragment(40.0, 12.0)],
     );
-
-    let block2 = LayoutNode::new(Style {
-        size: SizeStyle {
-            height: LengthOrAuto::Length(Length::Px(20.0)),
-            ..Default::default()
-        },
-        ..Default::default()
-    });
 
     let mut root = LayoutNode::with_children(
         Style {
@@ -144,123 +188,21 @@ fn block_with_interleaved_inline_and_block() {
             },
             ..Default::default()
         },
-        [inline1, block1, inline2, block2],
+        [inline1, inline2],
     );
 
     LayoutEngine::layout(&mut root, 800.0, 600.0);
 
-    assert_eq!(block_box(node(&root, 1)).border_box.y, 20.0);
+    let boxes1: Vec<BoxModel> = node(&root, 0).layout_box.iter().collect();
+    let boxes2: Vec<BoxModel> = node(&root, 1).layout_box.iter().collect();
 
-    let inline2_boxes: Vec<BoxModel> = node(&root, 2).layout_box.iter().collect();
-    assert_eq!(inline2_boxes[0].border_box.y, 50.0);
+    assert_eq!(boxes1[0].border_box.x, 0.0);
+    assert_eq!(boxes1[0].border_box.y, 0.0);
+    assert_eq!(boxes1[0].content_box.width, 35.0);
 
-    assert_eq!(block_box(node(&root, 3)).border_box.y, 70.0);
-    assert_eq!(block_box(&root).content_box.height, 90.0);
-}
+    assert_eq!(boxes2[0].border_box.x, 35.0);
+    assert_eq!(boxes2[0].border_box.y, 0.0);
+    assert_eq!(boxes2[0].content_box.width, 40.0);
 
-#[test]
-fn inline_fragments_between_block_children() {
-    let block1 = LayoutNode::new(Style {
-        size: SizeStyle {
-            height: LengthOrAuto::Length(Length::Px(20.0)),
-            ..Default::default()
-        },
-        ..Default::default()
-    });
-
-    let middle_fragments = LayoutNode::with_children(
-        Style {
-            display: Display::parse("inline").unwrap(),
-            line_height: Length::Px(16.0),
-            ..Default::default()
-        },
-        [fragment(50.0, 10.0), fragment(30.0, 10.0)],
-    );
-
-    let block2 = LayoutNode::new(Style {
-        size: SizeStyle {
-            height: LengthOrAuto::Length(Length::Px(30.0)),
-            ..Default::default()
-        },
-        ..Default::default()
-    });
-
-    let mut root = LayoutNode::with_children(
-        Style {
-            size: SizeStyle {
-                width: LengthOrAuto::Length(Length::Px(200.0)),
-                height: LengthOrAuto::Auto,
-                ..Default::default()
-            },
-            ..Default::default()
-        },
-        [block1, middle_fragments, block2],
-    );
-
-    LayoutEngine::layout(&mut root, 800.0, 600.0);
-
-    assert_eq!(block_box(node(&root, 0)).border_box.y, 0.0);
-
-    let middle_boxes: Vec<BoxModel> = node(&root, 1).layout_box.iter().collect();
-    assert_eq!(middle_boxes.len(), 1);
-    assert_eq!(middle_boxes[0].border_box.y, 20.0);
-
-    assert_eq!(block_box(node(&root, 2)).border_box.y, 36.0);
-    assert_eq!(block_box(&root).content_box.height, 66.0);
-}
-
-#[test]
-fn inline_fragments_then_block_then_more_fragments() {
-    let first_fragments = LayoutNode::with_children(
-        Style {
-            display: Display::parse("inline").unwrap(),
-            line_height: Length::Px(20.0),
-            ..Default::default()
-        },
-        [fragment(60.0, 10.0), fragment(50.0, 10.0)],
-    );
-
-    let block_child = LayoutNode::new(Style {
-        size: SizeStyle {
-            height: LengthOrAuto::Length(Length::Px(30.0)),
-            ..Default::default()
-        },
-        ..Default::default()
-    });
-
-    let more_fragments = LayoutNode::with_children(
-        Style {
-            display: Display::parse("inline").unwrap(),
-            line_height: Length::Px(20.0),
-            ..Default::default()
-        },
-        [fragment(40.0, 10.0)],
-    );
-
-    let mut root = LayoutNode::with_children(
-        Style {
-            size: SizeStyle {
-                width: LengthOrAuto::Length(Length::Px(100.0)),
-                height: LengthOrAuto::Auto,
-                ..Default::default()
-            },
-            ..Default::default()
-        },
-        [first_fragments, block_child, more_fragments],
-    );
-
-    LayoutEngine::layout(&mut root, 800.0, 600.0);
-
-    let first_boxes: Vec<BoxModel> = node(&root, 0).layout_box.iter().collect();
-    assert_eq!(first_boxes.len(), 2);
-    assert_eq!(first_boxes[0].border_box.y, 0.0);
-    assert_eq!(first_boxes[1].border_box.y, 20.0);
-
-    assert_eq!(block_box(node(&root, 1)).border_box.y, 40.0);
-
-    let more_boxes: Vec<BoxModel> = node(&root, 2).layout_box.iter().collect();
-    assert_eq!(more_boxes.len(), 1);
-    assert_eq!(more_boxes[0].border_box.y, 70.0);
-
-    assert_eq!(block_box(&root).content_box.height, 90.0);
+    assert_eq!(block_box(&root).content_box.height, 20.0);
 }
